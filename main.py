7dfbb70c4d118c75aabe4d77d4e7db1365401a61
VitaliyK1214 <vitaliy.korlyakov1412@gmail.com>
2026-07-10 14:40:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from routers.books import router as books_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
+	async with engine.begin() as conn:
 		await conn.run_sync(Model.metadata.create_all)
-    yield
+	yield
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(books_router)
