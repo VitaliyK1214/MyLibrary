f0cdfdbff19bb3981a713e0d0476ed0540d8071e
VitaliyK1214 <vitaliy.korlyakov1412@gmail.com>
2026-07-10 11:57:51 +0300

    Add files via upload

---
+++ schemas/books.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+class SBookBase(BaseModel):
+    title: str
+    author: str
+    year: int
+    pages: int = Field(ge=10)
+    is_read: bool = False
+
+class SBookAdd(SBookBase):
+    pass
+
+class SBook(SBookBase):
+    id: int
+    
+    model_config = ConfigDict(from_attributes=True)
