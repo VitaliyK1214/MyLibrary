33fd48762e68b60ce6edb1e33c88ecb11b837a9b
VitaliyK1214 <vitaliy.korlyakov1412@gmail.com>
2026-07-10 14:12:54 +0300

    Update books.py

--- routers/books.py
+++ routers/books.py
@@ -19,21 +19,21 @@ async def get_books(session: SessionDep):
 async def get_book(id: int, session: SessionDep):
     result = await BookRepository.get_book(id, session)
     if result is None:
-	raise HTTPException(
-	    status_code=status.HTTP_404_NOT_FOUND,
-	    detail="Книга не найдена"
-	)
+		raise HTTPException(
+	    	status_code=status.HTTP_404_NOT_FOUND,
+	    	detail="Книга не найдена"
+		)
     return result
 
 @router.put("/{id}", response_model=SBook)
 async def update_book(id: int, book: SBookAdd, session: SessionDep):
     result = await BookRepository.update_book(id, book, session)
     if result is None:
-	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return result
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(id: int, session: SessionDep):
     result = await BookRepository.delete_book(id, session)
     if result is None:
-	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
